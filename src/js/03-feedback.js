31790a651549edbcd9ff88558b65570d784273f5
Mykhalytskyi Vladyslav <vmykhali666@gmail.com>
2023-11-14 03:44:45 +0200

    small fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,7 +7,7 @@ const { email, message } = form.elements;
 form.addEventListener("input", throttle(event => {
     event.preventDefault();
     
-    var serializedData = {
+    let serializedData = {
         "email" : email.value,
         "message" : message.value
     };
@@ -15,15 +15,21 @@ form.addEventListener("input", throttle(event => {
 }, 500));
 
 form.addEventListener("submit", event => {
+    event.preventDefault();
+    let stored = localStorage.getItem(FFS_KEY);
+    if (stored != null) {
+        console.log(stored);
+    }
+    form.reset();
     localStorage.clear();
 });
 
 addEventListener("load", event => {
     try {
         let stored = localStorage.getItem(FFS_KEY);
-        let { email : saveMail, message : saveMess } = JSON.parse(stored);
-        email.value = saveMail;
-        message.value = saveMess;
+        let storedDTO = JSON.parse(stored);
+        email.value = storedDTO.email ?? "";
+        message.value = storedDTO.message ?? "";
     } catch (error) {
         form.reset();
     } 
